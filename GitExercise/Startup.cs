d293428fe671e5091884f9b717c02d5e7bd863eb
AlexKirilov7 <a.v.kirilov2020@pmg-pernik.com>
2024-10-10 11:15:09 +0300

    add 2

--- GitExercise/Startup.cs
+++ GitExercise/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace GitExercise
 {
@@ -6,6 +7,15 @@ namespace GitExercise
     {
         public static void Main()
         {
+            bool isAuthorized = CheckCredentials();
+
+            if (!isAuthorized)
+            {
+                Console.WriteLine("Acces denied.");
+                Console.ReadKey(intercept: true);
+                return;
+            }
+
             Console.WriteLine("Console Calculator App");
             Console.WriteLine(new string('-', 15));
 
@@ -35,10 +45,27 @@ namespace GitExercise
                 case "m":
                     OptionsManager.Multiply(a, b);
                     break;
+                case "d":
+                    OptionsManager.Divide(a,b);
+                    break;
+                case "sabs":
+                    OptionsManager.SubtractAbs(a,b);
+                    break;
             }
 
             Console.WriteLine("Pres any key to close the app...");
             Console.ReadKey(true);
+
         }
+        private static bool CheckCredentials()
+        {
+            Console.WriteLine(  "Enter password to gain access: ");
+            string password = Console.ReadLine();
+            Console.Clear();
+
+            return password == Password;
+        }
+
+        private const string Password = "abcd1234";
     }
 }
