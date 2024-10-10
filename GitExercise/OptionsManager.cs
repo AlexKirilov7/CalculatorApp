fddf0cc60dbd79f34faaae451ffac662dd6d599a
a-iv-dimitrov <alivdim@gmail.com>
2024-10-10 10:32:51 +0300

    Division with remainder added: Caluclations repetition

--- GitExercise/OptionsManager.cs
+++ GitExercise/OptionsManager.cs
@@ -7,7 +7,9 @@ namespace GitExercise
         public static string[] OptionsList = {
             "a - Add",
             "s - Subtract",
-            "m - Multiply"
+            "m - Multiply",
+            "dr - Divide Manager",
+            "ex - Exit"
         };
 
         public static void Add(double a, double b)
@@ -23,6 +25,11 @@ namespace GitExercise
         public static void Subtract(double a, double b)
         {
             Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+
+        internal static void DivideRemainder(double a, double b)
+        {
+            Console.WriteLine($"{a} % {b} = {a % b}");
         }
     }
 }
